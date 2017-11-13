9d27f0f662dfec6f7a0b77d4ccdb82172d64adff
markopolo <kuni@lean-system.co.jp>
2017-11-13 17:03:42 +0900

    Updated file structure

--- NewsClient/src/main/java/f1vote/news/App.java
+++ NewsClient/src/main/java/f1vote/news/App.java
@@ -1,8 +1,25 @@
 package f1vote.news;
 
+import android.app.Application;
+import android.content.Context;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
  * Created by markopolo on 07/07/2017.
  */
 
-public class App {
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Context context = null;
+
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/Helvetica_CE_Regular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
 }
